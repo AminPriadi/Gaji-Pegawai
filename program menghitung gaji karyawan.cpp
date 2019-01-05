329250a727158e03101ac4db57ddb8dfd9ad53e7
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 11:12:44 +0700

    Add files via upload

---
+++ program menghitung gaji karyawan.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float gaji_perjam=5000;
+    int gaji,jam;
+
+    cout<<"masukan jumlah jam kerja seorang pegawai ";
+    cin>>jam,
+
+    gaji=jam*gaji_perjam;
+    cout<<"maka jumlah gaji seorang pegawai adalah "<<gaji;
+
+    return 0;
+}
